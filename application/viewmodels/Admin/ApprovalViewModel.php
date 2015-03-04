d8716d60941ec1669982bb080c4a689871c7357e
Chenda Hoeung <chendahoeung@yahoo.ca>
2015-03-03 20:20:37 -0500

    start admin functions
    started buildin admin functions, added files for all forms needed for
    the admin panel.

---
+++ application/viewmodels/Admin/ApprovalViewModel.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* 
+*/
+class ApprovalViewModel extends ViewModel
+{
+	public $Id; //Id of story, or comment
+	public $Content; // Comment from admin
+	public $Approved; // if story is apporved or not
+
+	function __construct()
+	{		
+		$errors["Id"] = array(
+			'required' =>
+				array(
+					'Message' => gettext('The Id field is required!'),
+					'Properties' => array()
+				)
+		);
+		$errors["Content"] = array(
+			'required' =>
+				array(
+					'Message' => gettext('The Content field is required!'),
+					'Properties' => array()
+				)
+		);
+
+		$errors["Approved"] = array(
+			'required' =>
+				array(
+					'Message' => gettext('The Approved field is required!'),
+					'Properties' => array()
+				)
+		);
+
+		//Pass validation to the View Model
+		parent::__construct($errors);
+	}
+}
+?>
