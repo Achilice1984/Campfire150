5b17786424e1a246e9bcd8ce60b8730e9668594e
Joshua de Vries <josh.dvrs@gmail.com>
2015-03-28 22:46:01 -0400

    fix countdown style, fix actions taken button
    Coutdown background color no longer visible, actions taken was showing
    remove button even if not the owner.

--- application/views/Account/_actionsTaken.php
+++ application/views/Account/_actionsTaken.php
@@ -4,10 +4,13 @@
 	echo "<div style='font-size:1.2em; padding-left:10px;'>" . $action->Content . "</div>";
 ?>
 	<div class="actionTakenDiv">
-		<a style="float: left; margin-left:15px; margin-top:15px;" class="btn btn-danger removeAction" data-action="<?php echo BASE_URL . "account/removeAction/"; ?>" data-action-id="<?php echo $action->ActionsTakenId; ?>" href="#"><?php echo gettext("Remove"); ?></a>
-		<div style="float: left; margin-left:10px" id="ActionTakenSpinerDiv">
-			<?php include(APP_DIR . 'views/shared/_spinner_small.php'); ?>
-		</div>
+		<?php if ($currentUser->UserId == $action->user_UserId): ?>
+			
+			<a style="float: left; margin-left:15px; margin-top:15px;" class="btn btn-danger removeAction" data-action="<?php echo BASE_URL . "account/removeAction/"; ?>" data-action-id="<?php echo $action->ActionsTakenId; ?>" href="#"><?php echo gettext("Remove"); ?></a>
+			<div style="float: left; margin-left:10px" id="ActionTakenSpinerDiv">
+				<?php include(APP_DIR . 'views/shared/_spinner_small.php'); ?>
+			</div>
+		<?php endif ?>
 	</div>
 <?php
 	echo "</td></tr>";
