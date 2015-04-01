932ef9287db9beecfc4ddcee762eb1cf0f4699ca
Joshua de Vries <josh.dvrs@gmail.com>
2015-04-01 11:03:33 -0400

    remove ability to click on published stories when profile is privcate

--- application/views/Account/_publishedStories.php
+++ application/views/Account/_publishedStories.php
@@ -3,7 +3,7 @@
 	<h2 class="storyTitle"><?php echo $story->StoryTitle; ?></h2>
 	<div class="row">
 		<div class="col-md-4">
-			<?php if ($story->StoryPrivacyType_StoryPrivacyTypeId == 1) { ?>				
+			<?php if ($story->StoryPrivacyType_StoryPrivacyTypeId == 1 && $story->ProfilePrivacyType_PrivacyTypeId == 1) { ?>				
 			
 			    <a href="<?php echo BASE_URL . "story/display/" . $story->StoryId; ?>">
 			      	<img class="img-responsive" src="<?php echo image_get_path_basic($story->UserId, $story->PictureId, IMG_STORY, IMG_SMALL); ?>" alt="...">
